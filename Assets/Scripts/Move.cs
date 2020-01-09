155c031f193f7d7944b9fde4a537c4ce77ee15dd
José Francisco Riffo <jfriffoa@gmail.com>
2020-01-09 18:33:15 +0100

    Initiating git

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Vector2 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = GetComponent<SpriteRenderer>().bounds.size;
+    }
+
+    public void MoveInADir(int dir) {
+        var pos = transform.position;
+        switch (dir) {
+            case 0:
+                pos.x -= size.x;
+                break;
+            case 1:
+                pos.y += size.y;
+                break;
+            case 2:
+                pos.y -= size.y;
+                break;
+            case 3:
+                pos.x += size.x;
+                break;
+        }
+        transform.position = pos;
+    }
+}
